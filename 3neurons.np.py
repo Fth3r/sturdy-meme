30752c1a97f0cb7b44ff36c5ee5298ec5867c208
Jake Beckman <Jbeckman@cedarville.edu>
2021-09-23 08:10:34 -0500

    commenting

--- 3neurons.np.py
+++ 3neurons.np.py
@@ -8,6 +8,10 @@ weights = [[0.2,0.8,-0.5,1.0],
             [-0.26,-0.27,0.17,0.87]]
 biases = [2, 3, 0.5]
 
+# np.dot() does the heavy lifting of multiplying the matrix (weights)
+# by the array (inputs) and adding each bias in its array (vector addition).
+# Whichever vector is arg'd first determines the final shape
+# which in this case is a list of dot products (neuron outputs)
 layer_output = np.dot(weights, inputs) + biases
 
 print(layer_output)
